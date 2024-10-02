18580bef7dff1ec29a016dcc1277a58f0a2b40ad
Пётр Фирсов <peterfirsoff@gmail.com>
2024-10-02 21:25:22 +0300

    Добавил file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,9 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
+            options: {
+              outputPath: 'images/',
+            }
           },
         ],
       },
