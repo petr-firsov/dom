bca5243f75445a97d69b3a6d12256d13d796385d
Пётр Фирсов <peterfirsoff@gmail.com>
2024-10-07 14:03:07 +0300

    Импорт изображения гоблина

--- src/startgame.js
+++ src/startgame.js
@@ -1,5 +1,6 @@
 import {tiles} from './tiles';
+import image from './images/goblin.png';
 export function startGame() {
     let startTile = (Math.ceil(Math.random() * 15));
-    tiles[startTile].insertAdjacentHTML('beforeend', '<img src="./images/goblin.png" width=100 height=100 class="goblin" alt="Гоблин">');
+    tiles[startTile].insertAdjacentHTML('beforeend', `<img src="${image}" width=100 height=100 class="goblin" alt="Гоблин">`);
 }
